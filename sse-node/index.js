aaa1a8e7c4405655290492f4795667a7b62d9939
= <=>
2023-10-30 00:23:25 +0530

    completed

--- sse-node/index.js
+++ sse-node/index.js
@@ -3,7 +3,10 @@ const app = require('express')();
 const FootballScoreModel = require('./database/FootballSchema');
 const connToDB = require("./database/connection");
 const createMatch = require('./database/create-match');
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(require("express").json());
+const cors = require("cors");
 app.use(cors())
 connToDB();
 
@@ -15,15 +18,16 @@ const changeStream = FootballScoreModel.watch();
 
 app.get("/stream", (req, res) => {
     res.setHeader("Content-Type", "text/event-stream");
-
+   
     changeStream.on('change', (change) => {
         console.log("Changed Event : ", change.fullDocument);
-        res.json(change);
+        res.write(JSON.stringify(change.fullDocument));
     })
 })
 
 app.post("/post-data", (req, res) => {
-    const { teamA ,teamB, scoreA , scoreB } = req.body;
+    const { teamA ,teamB, scoreA ,scoreB } = req.body;
+    console.log(teamA)
     createMatch(teamA,teamB,scoreA,scoreB);
     res.redirect("/");
 })
